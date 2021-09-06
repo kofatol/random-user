4302d955282df615d96f7c7844de87e80321befa
kcherkasov <kcherkasov@emphasoft.com>
2021-09-06 23:31:53 +0300

    axios instance and getUsersRequest func created

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const CancelToken = axios.CancelToken;
+export let cancelSource: any;
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL
+});
+
+instance.interceptors.request.use((config) => {
+  if (config.method === "get") {
+    cancelSource = CancelToken.source();
+    config.cancelToken = cancelSource.token;
+  }
+  return config;
+});
+
+export function getUsersRequest() {
+  return instance.get("?results=50");
+}
