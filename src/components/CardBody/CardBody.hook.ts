bae9ee0749b88ba4455476932b76df773a65d2e5
kcherkasov <kcherkasov@emphasoft.com>
2021-09-06 23:30:23 +0300

    useUsersData hook created

---
+++ src/components/CardBody/CardBody.hook.ts
@@ -0,0 +1,27 @@
+import { User } from "./CardBody.model";
+import { useState, useEffect, useCallback } from "react";
+import { getUsersRequest, cancelSource } from "../../api";
+
+const useUsersData = () => {
+  const [usersData, setUsersData] = useState<User[]>([]);
+
+  const getUsersData = useCallback(async () => {
+    try {
+      const res = await getUsersRequest();
+      const { results } = await res.data;
+
+      setUsersData(results);
+    } catch (error) {
+      console.error("Something went wrong:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUsersData();
+    return cancelSource?.cancel("Operation canceled by the user.");
+  }, [getUsersData]);
+
+  return usersData;
+};
+
+export default useUsersData;
