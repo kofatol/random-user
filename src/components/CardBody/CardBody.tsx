4fe6eb1dec1a6fcb381bbffa18e889413d18ea74
kcherkasov <kcherkasov@emphasoft.com>
2021-09-07 00:32:12 +0300

    added UserInfo component to CardBody component

--- src/components/CardBody/CardBody.tsx
+++ src/components/CardBody/CardBody.tsx
@@ -1,4 +1,5 @@
 import useUsersData from "./CardBody.hook";
+import UserInfo from "../UserInfo/UserInfo";
 import CSS from "./CardBody.module.scss";
 
 const CardBody: React.FC = () => {
@@ -7,30 +8,9 @@ const CardBody: React.FC = () => {
   return (
     <div className={CSS.CardBody}>
       <ul id="result" className={CSS.CardBody__usersList}>
-        {usersData.map((user) => {
-          const id = user.registered.date;
-          const avatar = user.picture.medium;
-          const fullName = `${user.name.first} ${user.name.last}`;
-          const city = user.location.city;
-          const country = user.location.country;
-
-          return (
-            <li className={CSS.UserInfo} key={id}>
-              <img
-                src={avatar}
-                alt={`${fullName} avatar`}
-                className={CSS.UserInfo__image}
-              />
-              <div className={CSS.UserInfo__infoWrap}>
-                <h3 className={CSS.UserInfo__header}>{fullName}</h3>
-                <p className={CSS.UserInfo__text}>
-                  City: {city} <br />
-                  Country: {country}
-                </p>
-              </div>
-            </li>
-          );
-        })}
+        {usersData.map((user) => (
+          <UserInfo userData={user} key={user.registered.date} />
+        ))}
       </ul>
     </div>
   );
