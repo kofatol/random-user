14cb0326d6709a1d62c8154a37cf8b169983ff53
kcherkasov <kcherkasov@emphasoft.com>
2021-09-06 23:33:24 +0300

    usersData mapping added to CardBody component

--- src/components/CardBody/CardBody.tsx
+++ src/components/CardBody/CardBody.tsx
@@ -1,42 +1,36 @@
+import useUsersData from "./CardBody.hook";
 import CSS from "./CardBody.module.scss";
 
 const CardBody: React.FC = () => {
+  const usersData = useUsersData();
+
   return (
     <div className={CSS.CardBody}>
       <ul id="result" className={CSS.CardBody__usersList}>
-        <li className={CSS.UserInfo}>
-          <img
-            src="https://randomuser.me/api/portraits/women/3.jpg"
-            alt="Karen"
-            className={CSS.UserInfo__image}
-          />
-          <div className={CSS.UserInfo__infoWrap}>
-            <h3 className={CSS.UserInfo__header}>Karen Smith</h3>
-            <p className={CSS.UserInfo__text}>London, UK</p>
-          </div>
-        </li>
-        <li className={CSS.UserInfo}>
-          <img
-            src="https://randomuser.me/api/portraits/women/3.jpg"
-            alt="Karen"
-            className={CSS.UserInfo__image}
-          />
-          <div className={CSS.UserInfo__infoWrap}>
-            <h3 className={CSS.UserInfo__header}>Karen Smith</h3>
-            <p className={CSS.UserInfo__text}>London, UK</p>
-          </div>
-        </li>
-        <li className={CSS.UserInfo}>
-          <img
-            src="https://randomuser.me/api/portraits/women/3.jpg"
-            alt="Karen"
-            className={CSS.UserInfo__image}
-          />
-          <div className={CSS.UserInfo__infoWrap}>
-            <h3 className={CSS.UserInfo__header}>Karen Smith</h3>
-            <p className={CSS.UserInfo__text}>London, UK</p>
-          </div>
-        </li>
+        {usersData.map((user) => {
+          const id = user.registered.date;
+          const avatar = user.picture.medium;
+          const fullName = user.name.first + user.name.last;
+          const city = user.location.city;
+          const country = user.location.country;
+
+          return (
+            <li className={CSS.UserInfo} key={id}>
+              <img
+                src={avatar}
+                alt={`${fullName} avatar`}
+                className={CSS.UserInfo__image}
+              />
+              <div className={CSS.UserInfo__infoWrap}>
+                <h3 className={CSS.UserInfo__header}>{fullName}</h3>
+                <p className={CSS.UserInfo__text}>
+                  City: {city} <br />
+                  Country: {country}
+                </p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
