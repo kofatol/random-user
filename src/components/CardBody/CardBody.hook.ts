34bf445f5bf23e291df8f99cab4cdf95084a56df
kcherkasov <kcherkasov@emphasoft.com>
2021-09-10 01:14:14 +0300

    onUsersSearch, filterData,  visibleUsers state added to useUsersData hook

--- src/components/CardBody/CardBody.hook.ts
+++ src/components/CardBody/CardBody.hook.ts
@@ -4,24 +4,36 @@ import { getUsersRequest, cancelSource } from "../../api";
 
 const useUsersData = () => {
   const [usersData, setUsersData] = useState<User[]>([]);
+  const [visibleUsers, setVisibleUsers] = useState<User[]>([]);
+
+  const onUsersSearch = (e: React.ChangeEvent<HTMLInputElement>) => filterData(e.target.value);
+
+  const filterData = (searchTerm: string) => {
+    if (!searchTerm) {
+      return setVisibleUsers(usersData);
+    }
+
+    const matchedUsers: User[] = usersData.filter((user) =>
+      user.name.first.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setVisibleUsers(matchedUsers);
+  };
 
   const getUsersData = useCallback(async () => {
-    try {
-      const res = await getUsersRequest();
-      const { results } = await res.data;
+    const res = await getUsersRequest();
+    const { results } = await res.data;
 
-      setUsersData(results);
-    } catch (error) {
-      console.error("Something went wrong:", error);
-    }
+    setUsersData(results);
+    setVisibleUsers(results);
   }, []);
 
   useEffect(() => {
     getUsersData();
-    return cancelSource?.cancel("Operation canceled by the user.");
+    return cancelSource?.cancel("Operation canceled by user.");
   }, [getUsersData]);
 
-  return usersData;
+  return { usersData, visibleUsers, onUsersSearch };
 };
 
 export default useUsersData;
