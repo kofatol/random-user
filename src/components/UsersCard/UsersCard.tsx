68f2dcdbb2f95987f02f03c4e96a2cb46e7dcb04
kcherkasov <kcherkasov@emphasoft.com>
2021-09-10 01:19:46 +0300

    added using of useUsersData hook to UsersCard component

--- src/components/UsersCard/UsersCard.tsx
+++ src/components/UsersCard/UsersCard.tsx
@@ -1,12 +1,15 @@
-import CardHeader from "../CardHeader";
 import CardBody from "../CardBody";
+import CardHeader from "../CardHeader";
+import useUsersData from "./UsersCard.hook";
 import CSS from "./UsersCard.module.scss";
 
 const UsersCard: React.FC = () => {
+  const { visibleUsers, onUsersSearch } = useUsersData();
+
   return (
     <div className={CSS.UsersCard}>
-      <CardHeader />
-      <CardBody />
+      <CardHeader onInputChange={onUsersSearch} />
+      <CardBody users={visibleUsers} />
     </div>
   );
 };
