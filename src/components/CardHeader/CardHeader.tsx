d410dd80a1e172bf9130c88eed9ac38f3b96eadf
kcherkasov <kcherkasov@emphasoft.com>
2021-09-10 01:22:26 +0300

    added CardHeaderProps to CardHeader component

--- src/components/CardHeader/CardHeader.tsx
+++ src/components/CardHeader/CardHeader.tsx
@@ -1,17 +1,21 @@
+import { CardHeaderProps } from "./CardHeader.model";
 import CSS from "./CardHeader.module.scss";
 
-const CardHeader: React.FC = () => {
+const CardHeader: React.FC<CardHeaderProps> = (props) => {
+  const { onInputChange } = props;
+
   return (
     <header className={CSS.CardHeader}>
       <h2 className={CSS.CardHeader__title}>Random User Filter</h2>
       <small className={CSS.CardHeader__subtitle}>
-        Search by name and/or location
+        Search users by name
       </small>
       <input
         type="text"
         id="filter"
         placeholder="Search for a user"
         className={CSS.CardHeader__searchbar}
+        onChange={onInputChange}
       />
     </header>
   );
